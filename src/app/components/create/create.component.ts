a62ad040e497f4171022cb5e1d24b310ca0d1410
Manuel Caicedo <manuelcaicedo52@gmail.com>
2024-04-14 18:40:14 -0500

    add search

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { ServicioVideoJuegoService } from '../../service/servicio-video-juego.service';
+import {VideojuegoModel} from "../../model/videojuego.model";
 
 @Component({
     selector: 'app-create',
@@ -11,18 +12,32 @@ export class CreateComponent implements OnInit {
 
     isUpdate: boolean = false;
     formVideojuego: FormGroup = new FormGroup({});
+    @Input() videojuegoParaEditar: VideojuegoModel | null = null;
 
     constructor(private service: ServicioVideoJuegoService) { }
 
     ngOnInit(): void {
         this.formVideojuego = new FormGroup({
-            id: new FormControl('', [Validators.required, Validators.pattern('^[0-9]*$')]), 
+            id: new FormControl('', [Validators.required, Validators.pattern('^[0-9]*$')]),
             nombre: new FormControl('', [Validators.required]),
             precio: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+(\.[0-9]+)?$')]),
             multijugador: new FormControl('', [Validators.required]),
             fechaLanzamiento: new FormControl('', [Validators.required])
         });
+
+      if (this.videojuegoParaEditar) {
+        this.isUpdate = true;
+        this.formVideojuego.patchValue(this.videojuegoParaEditar);
+      }
     }
+
+  onSubmit() {
+    if (this.isUpdate) {
+      this.actualizarVideojuego();
+    } else {
+      this.crearVideojuego();
+    }
+  }
 
     crearVideojuego() {
         if (this.formVideojuego.valid) {
@@ -39,4 +54,26 @@ export class CreateComponent implements OnInit {
             }
         }
     }
+
+  actualizarVideojuego() {
+    if (this.formVideojuego.valid) {
+      const id = this.formVideojuego.get('id')?.value;
+      this.service.actualizarJuego(id, this.formVideojuego.value).subscribe({
+        next: (resp) => {
+          // Manejo de la respuesta
+        },
+        error: (e) => {
+          // Manejo del error
+        }
+      });
+    } else {
+      this.marcarControlesComoTocados();
+    }
+  }
+
+  private marcarControlesComoTocados() {
+    Object.values(this.formVideojuego.controls).forEach(control => {
+      control.markAsTouched();
+    });
+  }
 }
